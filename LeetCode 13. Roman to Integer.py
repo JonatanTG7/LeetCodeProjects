e178a35cd80e0aaf9747641b3a04a9f69eb8af16
Jonatan Gelerstain <jonatangeler7@gmail.com>
2024-11-02 21:02:14 +0200

    Add examples to LeetCode 13

--- LeetCode 13. Roman to Integer.py
+++ LeetCode 13. Roman to Integer.py
@@ -52,7 +52,22 @@ class Solution(object):
                 index += 1
         return roman_int
 
-x = "LVIII"
+x = "III"
 solution = Solution()
 result = solution.romanToInt(x)
 print(result)
+
+# Example 1:
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+
+# Example 2:
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+
+# Example 3:
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
